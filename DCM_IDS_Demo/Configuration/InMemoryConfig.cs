109dcfa19ddb572548f906adea37f2584792cdd5
priyalaruna <priyal@priyal.ai>
2023-07-12 18:03:27 +0530

    init

---
+++ DCM_IDS_Demo/Configuration/InMemoryConfig.cs
@@ -0,0 +1,83 @@
+using System.Security.Claims;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace DCM_IDS_Demo.Configuration;
+
+public class InMemoryConfig
+{
+    public static IEnumerable<IdentityResource> GetIdentityResources() =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource("state","DCM User State",new List<string>(){"state"})
+        };
+    
+    public static List<TestUser> GetUsers() =>
+        new List<TestUser>
+        {
+            new TestUser
+            {
+                SubjectId = "a83717d3-1800-d3a2-0a95-a8e69fab46f7",
+                Username = "priyal",
+                Password = "P@@$$W0rd",
+                Claims = new List<Claim>
+                {
+                    new Claim("full_name", "Priyal Aruna"),
+                    new Claim("state", "nsw"),
+                    new Claim("position", "doctor")
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "0960b3cc-ee94-7929-93a4-b6c1a9f13137",
+                Username = "gayan",
+                Password = "P@@$$W0rd",
+                Claims = new List<Claim>
+                {
+                    new Claim("full_name", "Gayan Gayan"),
+                    new Claim("state", "vic"),
+                    new Claim("position", "doctor")
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "0960b3cc-ee94-7929-93a4-b6c1a9f13137",
+                Username = "oshadha",
+                Password = "P@@$$W0rd",
+                Claims = new List<Claim>
+                {
+                    new Claim("full_name", "Oshadha Oshadha"),
+                    new Claim("state", "vic"),
+                    new Claim("position", "manager")
+                }
+            },
+            new TestUser
+            {
+                SubjectId = "0960b3cc-ee94-7929-93a4-b6c1a9f13137",
+                Username = "piyumi",
+                Password = "P@@$$W0rd",
+                Claims = new List<Claim>
+                {
+                    new Claim("full_name", "Piyumi Piyumi"),
+                    new Claim("state", "nsw"),
+                    new Claim("position", "nurse")
+                }
+            }
+        };
+    
+    
+    public static IEnumerable<Client> GetClients() =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "dcm-example",
+                ClientSecrets = new [] { new Secret("dcmsecret".Sha512()) },
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId, "full_name", "state", "position" }
+            }
+        };
+}
---
+++ DCM_IDS_Demo/Program.cs
@@ -0,0 +1,33 @@
+using DCM_IDS_Demo.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddIdentityServer()
+    .AddInMemoryIdentityResources(InMemoryConfig.GetIdentityResources())
+    .AddTestUsers(InMemoryConfig.GetUsers())
+    .AddInMemoryClients(InMemoryConfig.GetClients())
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
